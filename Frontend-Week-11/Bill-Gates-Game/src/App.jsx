f2960e912b041612a96250e2fc323c1062be9eae
İrem Kara Özdemir <iremkaraozdemir@gmail.com>
2024-08-30 00:12:35 +0300

    Refactored style of the Sell button.

--- Frontend-Week-11/Bill-Gates-Game/src/App.jsx
+++ Frontend-Week-11/Bill-Gates-Game/src/App.jsx
@@ -12,12 +12,12 @@ function App() {
   ]);
 
 
+
   const handleBuy = (index) => {
     const updatedProducts = [...products];
     const productPrice = updatedProducts[index].price;
     if (money >= productPrice) {
       let remainingAmount = productPrice;
-
       const interval = setInterval(() => {
         setMoney(prev => {
           if (remainingAmount > 0) {
@@ -36,7 +36,6 @@ function App() {
 
   const handleSell = (index) => {
     const updatedProducts = [...products];
-    const productPrice = updatedProducts[index].price;
     
     if (updatedProducts[index].count > 0) {
       const productPrice = updatedProducts[index].price;
@@ -67,20 +66,19 @@ function App() {
       <img className='bill-gates' src="/images/billgates.jpg" alt="" />
       <h1>Spend Bill Gates' Money</h1>
       </div>
-      <h1 className='money'>${money}</h1>
+      <h1 className='money'>${money.toLocaleString()}</h1>
       <div className='products'>
         {products.map((product,index) => (
           <div key={index} className='product'>
             <img src={product.img} alt="product.name"/>
             <h2 className='product-name'>{product.name}</h2>
-            <h3 className='price'>${product.price}</h3>
+            <h3 className='price'>${product.price.toLocaleString()}</h3>
             <div className='product-controls'>
-              <button onClick={() => handleSell(index)}> Sell </button>
+              <button onClick={() => handleSell(index)} style={{backgroundColor: product.count > 0 ? 'red': 'lightgray',color: product.count > 0 ? 'white': 'black'}}> Sell </button>
               <input type='text' value={product.count} readOnly/>
               <button onClick={() => handleBuy(index)} className='buy'>Buy</button>
             </div>
           </div>
-
         ))}
 
       </div>
